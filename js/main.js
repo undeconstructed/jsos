2d2c9d3a34ade0cf14bb2415c1c9d92d898171f6
Phil Housley <undeconstructed@gmail.com>
2019-03-28 00:06:03 +0000

    Commit something before bedtime

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 import OS from './os.js'
 
 let config = {
+  display: document.getElementById('display')
 }
 
 let os = new OS(config)
--- js/os.js
+++ js/os.js
@@ -13,16 +13,27 @@ let disk = {
       sys.debug('drv2 main')
     }
   },
-   // init app, and master of all other processes
-  'init': {
-    main (sys) {
-      sys.debug('init main')
-    }
-  },
   // fs server
   'fs': {
     main (sys) {
       sys.debug('fs main')
+      sys.call('register', null, null, ['fs', 'incoming'])
+    },
+    incoming (sys) {
+      sys.debug('fs incoming')
+    }
+  },
+   // init app, and master of all other pods
+  'init': {
+    main (sys) {
+      sys.debug('init main')
+      sys.call('connect', 'fsconnected', 'ret1', ['fs'])
+    },
+    fsconnected (sys) {
+      let fschn = sys.read('ret1')
+      sys.debug('init fsconnected', fschn)
+      sys.write('fschn', fschn)
+      sys.send(fschn, 'some message')
     }
   },
   // display server
@@ -36,31 +47,39 @@ let disk = {
     main (sys) {
       sys.debug('gshell main')
     }
-  }
+  },
+  // what to start up
+  'inittab': `display
+`
 }
 
 // the os kernel.
 export default class OS {
   constructor (config) {
     this.config = config
+    this.display = config.display
     this.disk = disk
   }
   boot () {
     this.time = 0
-    this.process0 = this.createProcess0()
-    this.process0.start()
+    this.pod0 = this.createPod0()
+    this.pod0.start()
+    this.display.textContent = 'booting'
     // tick
     window.setTimeout(() => this.tick(), 0)
   }
-  createProcess0 () {
-    // process0 code is embedded in kernel
-    let p = new Process(null, {
+  createPod0 () {
+    // pod0 code is embedded in kernel
+    let p = new Pod(this, null, {
       main (sys) {
         sys.debug('0 main')
-        sys.call('spawn', ['drv1'])
-        sys.call('spawn', ['drv2'])
-        sys.call('spawn', ['fs'])
-        sys.call('spawn', ['init'])
+        sys.call('spawn', 'cb', null, ['drv1'])
+        sys.call('spawn', 'cb', null, ['drv2'])
+        sys.call('spawn', 'cb', null, ['fs'])
+        sys.call('spawn', 'cb', null, ['init'])
+      },
+      cb (sys) {
+        sys.debug('0 cb')
       }
     })
     p.id = 0
@@ -69,7 +88,7 @@ export default class OS {
   tick () {
     this.time++
 
-    let res = this.process0.tick(this)
+    let res = this.pod0.tick(this)
 
     for (let call of res.calls) {
       switch (call.name) {
@@ -80,16 +99,23 @@ export default class OS {
 
     window.setTimeout(() => this.tick(), 100)
   }
+  debug (args) {
+    this.display.textContent += `\n${this.time} ${args}`
+  }
 }
 
 // fully isolated world in which work can be done
-class Process {
-  constructor (parent, binary) {
+class Pod {
+  constructor (os, parent, binary) {
+    this.os = os
     this.parent = parent
     this.binary = binary
     // state
+    this.channel = new Channel()
+    this.names = new Map()
     this.queue = []
     this.states = new IdMap()
+    this.channels = new IdMap()
     // children
     this.children = new IdMap()
   }
@@ -97,89 +123,152 @@ class Process {
     let sid = this.states.add(new State())
     this.enqueue('main', sid)
   }
-  spawn (binary) {
-    // cheat straight to disk
-    let bin = disk[binary]
-    let p = new Process(this, bin)
-    this.children.add(p)
-    p.start()
-  }
   enqueue (entry, state) {
     this.queue.push(new Task(entry, state))
   }
   tick (ctx) {
-    let calls = []
+    for (let channel of this.channels.values()) {
+    }
+
     for (let c of this.children.values()) {
       let res = c.tick(this)
+      this.doSends(res.sends)
+      // TODO - pass these up?
       for (let call of res.calls) {
-        call.process = c
-        if (!this._syscall(call)) {
-          calls.push(call)
-        }
+        console.log('unhandled', call)
       }
     }
 
-    if (this.queue.length > 0) {
-      let t = this.queue.shift()
-      let res = this.invoke(t)
-
-      if (this.parent === null) {
-        // only process 0 is allowed to work on its own syscalls
-        for (let call of res.calls) {
-          call.process = this
-          if (!this._syscall(call)) {
-            calls.push(call)
-          }
-        }
-      } else {
-        for (let call of res.calls) {
-          call.process = this
-          calls.push(call)
-        }
-      }
+    if (this.queue.length === 0) {
+      return { sends: [], calls: [] }
     }
 
-    return { calls }
+    let t = this.queue.shift()
+    let res = this.invoke(t)
+
+    let unhandledCalls = this.doSyscalls(res.calls)
+
+    return { sends: res.sends, calls: unhandledCalls }
   }
   invoke (task) {
-    let s = this.states.get(task.state)
+    let os = this.os
+    let state = this.states.get(task.state)
     let calls = []
+    let sends = []
+
     let c = {
-      mem: s,
-      debug: console.log,
-      call: (name, args) => {
-        calls.push({ name, args })
+      debug (...args) {
+        os.debug(args)
+        console.log(...args)
+      },
+      read (addr) {
+        return JSON.parse(state.data.get(addr))
+      },
+      write (addr, data) {
+        state.data.set(addr, JSON.stringify(data))
+      },
+      call: (name, reentry, tag, args) => {
+        calls.push([name, reentry, tag, JSON.stringify(args)])
+      },
+      send: (chn, data) => {
+        sends.push([chn, JSON.stringify(data)])
       }
     }
-    let e = this.binary[task.entry]
+
+    let entry = this.binary[task.entry]
     try {
       // this is user space
-      e(c)
+      entry.call(null, c)
     } catch (e) {
       console.log('userspace error', e)
     }
-    return { calls }
+
+    calls = calls.map(e => ({ pod: this, state: state, call: e[0], reentry: e[1], tag: e[2], args: e[3] }))
+    sends = sends.map(e => ({ pod: this, channel: e[0], data: e[1] }))
+
+    return { state, calls, sends }
   }
-  _syscall (call) {
-    switch (call.name) {
+  doSyscalls (calls) {
+    let unhandled = []
+    for (let call of calls) {
+      call.pod = this
+      call.args = JSON.parse(call.args)
+      let ret = this.doSyscall(call)
+      if (ret !== null) {
+        if (call.tag) {
+          call.state.data.set(call.tag, ret)
+        }
+        if (call.reentry) {
+          this.enqueue(call.reentry, call.state.id)
+        }
+      } else {
+        unhandled.push(call)
+      }
+    }
+    return unhandled
+  }
+  doSyscall (call) {
+    switch (call.call) {
     case 'spawn':
-      this._spawn(call.process, call.args)
-      return true
+      return this._spawn(call.pod, call.args)
+    case 'register':
+      return this._register(call.pod, call.args)
+    case 'connect':
+      return this._connect(call.pod, call.args)
     default:
-      return false
+      return null
     }
   }
-  _spawn (parent, args) {
+  _spawn (pod, args) {
     let [binary] = args
-    parent.spawn(binary)
+
+    // cheat straight to disk
+    let binx = disk[binary]
+    let bin = {}
+    for (let prop in binx) {
+      bin[prop] = binx[prop]
+    }
+
+    let p = new Pod(this.os, this, bin)
+    this.children.add(p)
+    p.start()
+    return p.id
+  }
+  _register (pod, args) {
+    let [name] = args
+    pod.parent.names.set(name, pod)
+    return name
+  }
+  _connect (pod, args) {
+    let [name] = args
+    let tgt = pod.parent.names.get(name)
+    let chn = new Channel()
+    this.channels.add(chn)
+    return chn.id
+  }
+  doSends (sends) {
+    for (let send of sends) {
+      let tgt = this.channels.get(send.channel)
+      // this.os.debug('send', send)
+    }
   }
 }
 
-// local memory, to be accessed by at most one thing at a time
+// local memory, to be accessed by at most one task at a time
 class State {
   constructor () {
     this.data = new Map()
     this.owner = null
+  }
+}
+
+// connects pods
+class Channel {
+  constructor (start, end) {
+    this.start = start
+    this.end = end
+    this.up = []
+    this.down = []
   }
 }
 
